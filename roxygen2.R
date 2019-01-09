7db3f89377b8f1ae93dc6d305ef2b09320c9b196
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2019-01-09 20:05:44 +0100

    Start roxygen commands in package directory

--- roxygen2.R
+++ roxygen2.R
@@ -8,24 +8,23 @@ library(roxygen2)
 ##
 ## (2) Create documentation file(s) in subdirectory testroxygen/man
 ##
-# roxygenise("testroxygen") # Only consider files in subdirectory testroxygen/R
-document("diagmeta") # Also considers datasets in subdirectory diagmeta/data
+document("../diagmeta") # Also considers datasets in subdirectory diagmeta/data
 
 
 ##
 ## (3) Build R package and PDF file with help pages
 ##
-build("diagmeta")
-build_manual("diagmeta")
+build("../diagmeta")
+build_manual("../diagmeta")
 
 
 ##
 ## (4) Install R package
 ##
-install("diagmeta")
+install("../diagmeta")
 
 
 ##
 ## (5) Check R package
 ##
-check("diagmeta")
+check("../diagmeta")
